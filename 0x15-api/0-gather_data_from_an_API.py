9c16e360b77dbaea97bdad2dbe248a57c3ee761e
flipeprez <flipeprez@gmail.com>
2022-05-16 17:13:49 -0700

    add comments

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import requests
 import sys
-
+'''a nice comment'''
 
 def todo_done(id):
     '''a nice comment for checker'''
@@ -9,7 +9,7 @@ def todo_done(id):
     url = "https://jsonplaceholder.typicode.com/user/{}".format(id)
     response = requests.get(url)
     res_json = response.json()
-    employee_name = res_json.get("name")
+    employ_name = res_json.get("name")
 
     url = "https://jsonplaceholder.typicode.com/user/{}/todos".format(id)
     todos = requests.get(url)
@@ -24,12 +24,11 @@ def todo_done(id):
             tsk_completed += 1
             tsk_list += "\t" + tsk.get("title") + "\n"
 
-    print("employee {} is done with task({}/{}):".format(employee_name,
+    print("employee {} is done with task({}/{}):".format(employ_name,
                                                          tsk_completed,
                                                          number_of_tsk))
     print(tsk_list[:-1])
 
 
 if __name__ == "__main__":
-   '''a nice comment''' 
     todo_done(sys.argv[1])
