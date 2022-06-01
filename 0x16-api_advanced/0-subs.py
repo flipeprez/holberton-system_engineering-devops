d3be30d7d5149493e8d0f70205b9cdb60c9762fb
flipeprez <flipeprez@gmail.com>
2022-05-31 17:57:00 -0700

    fix pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 ''' a nice comment'''
-import request 
+import request
 import json
 
 
-def number_of_suscribers(subreddit)
+def number_of_suscribers(subreddit):
     ''' a nice comments'''
-    header = {'user-agent':'mozilla/100.0.2'}
+    header = {'user-agent': 'mozilla/100.0.2'}
     url = 'https://api.reddit.com/r/{subreddit}/about.json'.format(subreddit)
     response = request.get(url, headers=header)
     if response.status_code = 200:
@@ -14,4 +14,3 @@ def number_of_suscribers(subreddit)
     else:
         sub = 0
     return sub
-
