1a949476d5935b15ebe641abc3523556d8fc6057
flipeprez <flipeprez@gmail.com>
2022-05-31 19:07:24 -0700

    update func

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 ''' a nice comment'''
 import request
-import json
 
 
 def number_of_suscribers(subreddit):
@@ -9,7 +8,7 @@ def number_of_suscribers(subreddit):
     header = {'user-agent': 'mozilla/100.0.2'}
     url = 'https://api.reddit.com/r/{subreddit}/about.json'.format(subreddit)
     response = request.get(url, headers=header)
-    if response.status_code = 200:
+    if response.status_code == 200:
         sub = response.json()['data']['suscribers']
     else:
         sub = 0
