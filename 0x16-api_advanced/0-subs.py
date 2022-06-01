e9345cbca6f7479e96b3e851c794d7351c1e4158
flipeprez <57754545+flipeprez@users.noreply.github.com>
2022-05-31 22:57:42 -0300

    fix import request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,15 @@
-#!/usr/bin/python3
-''' a nice comment'''
-import request
-import json
+from importlib import import_module
+import requests
 
 
-def number_of_suscribers(subreddit):
-    ''' a nice comments'''
-    header = {'user-agent': 'mozilla/100.0.2'}
-    url = 'https://api.reddit.com/r/{subreddit}/about.json'.format(subreddit)
-    response = request.get(url, headers=header)
-    if response.status_code = 200:
-        sub = response.json()['data']['suscribers']
+def number_of_subscribers(subreddit):
+    ''' Finds the number of subscribers in a subreddit '''
+
+    header = {'User-Agent':  'mozilla/100.0.2'}
+    url = "https://api.reddit.com/r/{subreddit}/about.json".format(subreddit)
+    response = requests.get(url, headers=header)
+    if response.status_code == 200:
+        subs = response.json()["data"]["subscribers"]
     else:
-        sub = 0
-    return sub
+        subs = 0
+    return subs
